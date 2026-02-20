837ed8455e22ccd0b4ac4538335b74745724db5b
Efraim Fonseca <efraimfonseca@gmail.com>
2026-02-19 23:40:24 -0300

    First commit

---
+++ src/main/java/br/com/alura/gptjava/TestaIntegracao.java
@@ -0,0 +1,9 @@
+package br.com.alura.gptjava;
+
+public class TestaIntegracao {
+
+    public static void main(String[] args) {
+        String chatUser = "Gere 5 produtos produtos";
+        String chatSystem = "Voçê é um criador de podutos fictícios para um eccomrerce, gere os produtos com nome, descrição e preço. Todos os produtos devem ser do nicho de pets. Gere os produtos em formato json, seguindo o exemplo: [{\"nome\": \"Ração para Cães\", \"descrição\": \"Ração de alta qualidade para cães de todas as idades.\", \"preço\": 150.00}]";
+    }
+}
